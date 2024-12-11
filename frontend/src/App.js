d875efe4fca420d4b84017efd6675c63d4e451dd
rodgers-munene <munenerodgers72@gmail.com>
2024-12-11 15:22:42 +0300

    frontend final folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App w-screen h-screen flex justify-center items-center">
+      <h1 className='font-extrabold text-3xl'>WELCOME TO AEDO SHOPPING</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
